6abad984bda52ca7d53fb2a58da0012e9cd3aeba
antonraguzin <raguzin82@gmail.com>
2022-05-26 16:28:30 +0300

    added new price

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
-        System.out.println(miles);
-    }
+        System.out.println("Цена 10 000 руб.");
+        System.out.println(miles + " Бонусов");
+
+        System.out.println("Цена 12 000 руб.");
+        System.out.println(service.calculate(12_000) + " Бонусов");
+            }
 }

aa38b20463330ee295471aeb84eb63c6032c7431
antonraguzin <raguzin82@gmail.com>
2022-05-26 16:05:10 +0300

    calc. working

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 25700; //стоимость билета в рублях
-        int oneMile = 20; // количество рублей за 1 милю
-        int milesQuantity = (ticketPrice / oneMile); // количество миль за билет
-        System.out.println("Начислено бонусных миль - " + milesQuantity);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
